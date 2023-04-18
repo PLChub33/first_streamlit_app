9e393f269f1d6a44bbe7566e8d844cc4edb04221
PLChub33 <131195121+PLChub33@users.noreply.github.com>
2023-04-18 15:01:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header ('🍌🥭 Build YOur Onw Fruit Smoothie 🥝🍇')
+streamlit.header ('🍌🥭 Build YOur Own Fruit Smoothie 🥝🍇')
 
