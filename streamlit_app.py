96195c9d16455ae9e61dc91f27d98e3655c61a22
PLChub33 <131195121+PLChub33@users.noreply.github.com>
2023-04-20 16:55:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,6 @@ except URLError as e:
   streamlit.error()
 
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
     
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -53,12 +50,23 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake-related functions
+def get_fruit_load_list();
+with my_cnx.cursor() as my_cur
+my_cur.execute("SELECT * from fruit_load_list")
+return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlist.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
+  # don't run anything past here while we troubleshoot
+streamlit.stop()
+  
+
 
 #Add a second test entry box
 streamlit.header("What fruit would you like to add")
